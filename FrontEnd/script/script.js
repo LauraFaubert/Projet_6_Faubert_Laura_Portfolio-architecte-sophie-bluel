e775b3b8e6bd02ba5dc7ed4548aa7a15020ca854
LauraFaubert <147067205+LauraFaubert@users.noreply.github.com>
2024-04-23 15:23:55 +0200

    2ème commit  remise à zéro de la gallerie et récupération des éléments dans la gallerie via le back end

--- FrontEnd/script/script.js
+++ FrontEnd/script/script.js
@@ -1,44 +1,59 @@
-
-
+let works
+//// récupération de l'élément du DOM qui accueillera les photos
+const divGallery = document.querySelector('.gallery');
+//console.log(divGallery)
 // Récupération des travaux depuis le back-end avec fetch
 
-async function getWork (){
+genererWork();
+async function genererWork (){
+    try{
+        const response = await fetch('http://localhost:5678/api/works');
 
-    const reponse = await fetch('http://localhost:5678/api/works');
-    const works = await reponse.json();
+        if (!response.ok){
+            throw new Error ('Erreur lors de la récupération des données');
+        }
+        const data = await response.json(); 
+        works = data;
+
+        console.log(works);
+
+        creerGallery();
+
+        } catch (error){
+            console.error ('Une erreur s\'est produite',error);
+        }
 }
 
-getWork();
 
 
+function creerGallery (){
+    
+    resetDivGallery();
 
+        for (let i = 0; i < works.length; i++){
+            const gallery = works[i];
+            // Création d'une balise dédié à la photo
+            const figureElement = document.createElement ("figure");
+            figureElement.dataset.id = works[i].id;
+            // Création des autres balises
+            const imgElement = document.createElement("img");
+            imgElement.src = gallery.imageUrl;
+            const figcaptionElement = document.createElement ("figcaption");
+            figcaptionElement.textContent = gallery.title;
+    
+            //ajout de img et figcaption à figure 
+            figureElement.appendChild(imgElement);
+            figureElement.appendChild (figcaptionElement);
+    
+            //ajout de figure à gallery
+            divGallery.appendChild(figureElement);
+    
+        }
 
-function genererWorks (works) {
-    for (let i = 0; i < works.lenght; i++){
-        const gallery = works[i];
-
-        // récupération de l'élément du DOM qui accueillera les photos
-        const divGallery = document.querySelector('.gallery');
-        //console.log(divGallery)
-        //Vider la gallery existante
-        divGallery.innerHTML="";
-        // Création d'une balise dédié à la photo
-        const figureElement = document.createElement ("figure");
-        figureElement.dataset.id = works[i].id;
-        // Création des autres balises
-        const imgElement = document.createElement("img");
-        imgElement.src = gallery.imageUrl;
-        const figcaptionElement = document.createElement ("figcaption");
-        figcaptionElement.src = gallery.title;
-
-        //ajout de img et figcaption à figure 
-        figureElement.appendChild(imgElement);
-        figureElement.appendChild (figcaptionElement);
-
-        //ajout de figure à gallery
-        gallery.appendChild(figureElement);
-
-    }
 }
-genererWorks();
+
+function resetDivGallery (){
+    //Vider la gallery existante
+    divGallery.innerHTML="";
+}
 
