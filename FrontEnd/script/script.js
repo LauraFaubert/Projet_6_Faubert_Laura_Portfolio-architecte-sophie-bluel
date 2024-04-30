8b932db12078e116b9b3fa42fb2ffd518dee7083
LauraFaubert <147067205+LauraFaubert@users.noreply.github.com>
2024-04-30 14:10:13 +0200

    Etape 1 terminée générer la gallerie et créer les filtres pour cette gallerie

--- FrontEnd/script/script.js
+++ FrontEnd/script/script.js
@@ -1,10 +1,5 @@
-let works
-//// récupération de l'élément du DOM qui accueillera les photos
-const divGallery = document.querySelector('.gallery');
-//console.log(divGallery)
 // Récupération des travaux depuis le back-end avec fetch
 
-genererWork();
 async function genererWork (){
     try{
         const response = await fetch('http://localhost:5678/api/works');
@@ -12,12 +7,12 @@ async function genererWork (){
         if (!response.ok){
             throw new Error ('Erreur lors de la récupération des données');
         }
-        const data = await response.json(); 
-        works = data;
+        const works = await response.json(); 
+        
+        //console.log(works);
 
-        console.log(works);
-
-        creerGallery();
+        creerGallery(works);
+        creerFiltreBouttons(works);
 
         } catch (error){
             console.error ('Une erreur s\'est produite',error);
@@ -25,35 +20,78 @@ async function genererWork (){
 }
 
 
-
-function creerGallery (){
+//fonction pour créer la galerie des travaux
+function creerGallery (works){
+    const divGallery = document.querySelector('.gallery');
     
-    resetDivGallery();
+    supprimerDivGallery(divGallery);
 
-        for (let i = 0; i < works.length; i++){
-            const gallery = works[i];
-            // Création d'une balise dédié à la photo
-            const figureElement = document.createElement ("figure");
-            figureElement.dataset.id = works[i].id;
-            // Création des autres balises
-            const imgElement = document.createElement("img");
-            imgElement.src = gallery.imageUrl;
-            const figcaptionElement = document.createElement ("figcaption");
-            figcaptionElement.textContent = gallery.title;
-    
-            //ajout de img et figcaption à figure 
-            figureElement.appendChild(imgElement);
-            figureElement.appendChild (figcaptionElement);
-    
-            //ajout de figure à gallery
-            divGallery.appendChild(figureElement);
-    
-        }
+    works.forEach(work => {
+        const figureElement = document.createElement("figure");
+        figureElement.dataset.id = work.id;
 
-}
+        const imgElement = document.createElement("img");
+        imgElement.src = work.imageUrl;
 
-function resetDivGallery (){
-    //Vider la gallery existante
+        const figcaptionElement = document.createElement("figcaption");
+        figcaptionElement.textContent = work.title;
+
+        figureElement.appendChild(imgElement);
+        figureElement.appendChild(figcaptionElement);
+
+        divGallery.appendChild(figureElement);
+    });
+}    
+//Fonction pour supprimer la galerie existante
+function supprimerDivGallery (divGallery){
     divGallery.innerHTML="";
 }
 
+
+//Fonction pour créer les boutons de filtre
+
+async function creerFiltreBouttons(works){
+    
+    const divFilters = document.createElement ("div");
+    divFilters.classList.add("filters")
+
+    const boutonsNoms = ["Tous", "Objets","Appartements","Hotels & restaurants"];
+
+    boutonsNoms.forEach((nom, index)=> {
+        const categorieBouton = document.createElement ("button");
+        categorieBouton.textContent = nom;
+        divFilters.appendChild(categorieBouton);
+
+        categorieBouton.addEventListener("click", () => {
+            let categoryId = null;
+            if (index > 0){
+                categoryId = index;
+            }
+            filtreTravauxParCategories(works, categoryId);
+
+            divFilters.querySelectorAll("button").forEach(button => {
+                button.classList.remove("bouton_actif");
+            });
+            categorieBouton.classList.add("bouton_actif");
+        });
+    });
+    // Insérez les boutons de filtre après le H2 de la section
+    const h2Element = document.querySelector("#portfolio h2")
+    h2Element.insertAdjacentElement("afterend", divFilters); 
+}
+// Fonction pour filtrer les travaux par catégorie
+function filtreTravauxParCategories (works, categoryId){
+    let filtreWorks;
+    if (categoryId !== null){
+        filtreWorks = works.filter (work => work.category.id === categoryId);
+    }else{
+        filtreWorks = works;
+    }
+    
+    
+    creerGallery(filtreWorks);
+   
+}
+
+
+genererWork();
